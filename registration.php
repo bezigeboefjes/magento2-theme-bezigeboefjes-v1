24c642e1b8bedceefc98a4ac3f27a71105411462
Bezigeboefjes <git@bezigeboefjes.nl>
2017-07-14 15:21:43 +0200

    Update registration files

--- registration.php
+++ registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/bezigeboefjes/bbtheme1',
+    'frontend/bezigeboefjes/theme-bezigeboefjes-v1',
     __DIR__
 );
